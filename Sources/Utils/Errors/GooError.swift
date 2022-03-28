1b4a32902f4860d25c8dd4c8d2a84c690c1c01f1
Antonio Rodríguez <antrorc@gmail.com>
2022-03-28 19:35:42 +0200

    Error public access

--- Sources/Utils/Errors/GooError.swift
+++ Sources/Utils/Errors/GooError.swift
@@ -4,17 +4,17 @@ import SwiftUI
 @available(iOS 15.0, *)
 public struct GooError: Error {
 
-    enum ErrorType {
+    public enum ErrorType {
         case fatal
         case regular
         case warning
         case noError
     }
 
-    var title: LocalizedStringKey = "error_generic_title"
-    var desc: LocalizedStringKey = "error_generic_descr"
-    var dismissButton: LocalizedStringKey = "error_dismissButton"
-    var error: ErrorType = .noError
-    var alert: Bool = false
+    public var title: LocalizedStringKey = "error_generic_title"
+    public var desc: LocalizedStringKey = "error_generic_descr"
+    public var dismissButton: LocalizedStringKey = "error_dismissButton"
+    public var error: ErrorType = .noError
+    public var alert: Bool = false
 
 }
