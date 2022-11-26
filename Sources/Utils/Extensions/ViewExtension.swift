f4d2797c98b936857b24b01b0e5de77825ef7231
Antonio Rodríguez <antrorc@gmail.com>
2022-11-26 18:06:21 +0100

    Fix errors

--- Sources/Utils/Extensions/ViewExtension.swift
+++ Sources/Utils/Extensions/ViewExtension.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 #if !os(macOS)
 
+@available(iOS 15.0, *)
 extension View {
     func hideKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
